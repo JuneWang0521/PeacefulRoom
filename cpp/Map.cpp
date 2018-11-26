181de604ed49f92d0646b0fb1110294c16fa0eb2
JuneWang0521 <813954946@qq.com>
2018-11-26 18:55:15 +0800

    撒打算

--- cpp/Map.cpp
+++ cpp/Map.cpp
@@ -10,6 +10,22 @@ Map::Map()
 	m_iMapWidth = 1600;
 }
 
+void Map::MapMove()
+{
+	if (GameEngine::GetEngine()->GetCharacterNoMove() == FALSE || ((KEYDOWN(65) || KEYDOWN(97)) && (KEYDOWN(68) || KEYDOWN(100))))  //人物可动，图不可动(ps:防双键同时按下的BUG)
+	{
+		m_iBGposition = m_iBGposition;
+	}
+	else if (GameEngine::GetEngine()->GetCharacterNoMove() == TRUE && (KEYDOWN(65) || KEYDOWN(97))) //地图左移
+	{
+		m_iBGposition -= 10;
+	}
+	else if (GameEngine::GetEngine()->GetCharacterNoMove() == TRUE && (KEYDOWN(68) || KEYDOWN(100))) //地图右移
+	{
+		m_iBGposition += 10;
+	}
+}
+
 BOOL MapInitial()
 {
 	Map *g_pMap = new Map();
